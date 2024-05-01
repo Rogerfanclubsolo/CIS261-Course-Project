959593b904063d204dc13ef3aafc95edec47b0c8
username <useremail>
2024-04-30 20:13:10 -0700

    Gang But not Great

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -54,6 +54,63 @@ while True:
     totalNetPay += netPay
     PrintTotals(totalEmployees, totalHours, totalGrossPay, totalTax, totalNetPay)
     
+    def getDatesWorked():
+        fromDate = input("Please enter start date in the following format MM/DD/YYYY: ")
+        endDate = input("Please enter end date in the following format MM/DD/YYYY: ")
+        return fromDate, endDate
+
+#this function means xyz
+def getEmpName():
+    empName = input("Enter employee name: ")
+    return empName
+
+def getHoursWorked():
+    hours = float(input("Enter Hours: "))
+    return hours
+
+def getHourlyRate():
+    HourlyRate = float(input("Enter Hourly Rate: "))
+    return hourlyRate 
+
+def getTaxRate():
+    taxRate = float(input("Enter Tax Rate: "))
+    taxRate = taxRate / 100
+    return taxRate
+
+def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
+    gPay = hours * hourlyRate
+    incomeTax = gPay * taxRate
+    netPay = gPay - incomeTax
+    return gPay, incomeTax, netPay
+
+def printInfo(empDetailList):
+    totalEmployees = 0 
+    totalHours = 0.00
+    totalGrossPay = 0.00
+    totalTax = 0.00
+    totalNetPay = 0.00
+    for empList in empDetailList:
+        fromDate = empList[0]
+        endDate = empList[1]
+        empName = empList[2]
+        hours = empList[3]
+        hourlyRate = empList[4]
+        taxRate = empList [5]
+        
+    grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyRate, taxRate)
+    print(fromDate, endDate, empName, f"{hours:,.2f}", f"{hourlyRate:,.2f}", f"{grosspay:,.2f}", f"{taxRate:,.1%}", f"{incometax:,.2f}", f"{netpay:,.2f}") 
+
+    totalEmployees  += 1 
+    totalHours += 1
+    totalGrossPay += grosspay
+    totalTax += incometax 
+    totalNetPay  += netpay
+
+
+    
+                    
+
+
     
     
 
